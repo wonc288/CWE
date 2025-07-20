2fc605d8182a48500c76ab6d6dc9ca4b99aa7b5a
wonc288 <155019702+wonc288@users.noreply.github.com>
2025-07-20 14:29:24 -0400

    Update template.py

--- template.py
+++ template.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python3 
+import subprocess
+from pwn import *
 
+binary_path = './' # exe
+libc_path = ''
+remote_host = ''
+remote_port = 0  # <-- Needs to be a valid integer if using REMOTE
+
+context.binary = binary_path
+#context.terminal = ['tmux', 'splitw', '-h']
+elf = context.binary
+#libc = ELF(libc_path)
+rop = ROP(elf)
+libc = ELF(libc_path)
+
+if args.GDB:
+    p = gdb.debug(binary_path, gdbscript='b *')
+elif args.REMOTE:
+    p = remote(remote_host, remote_port)
+else:
+    p = process(binary_path)
+
+def main():
+    
+
+    p.sendline(payload)
+
+    p.interactive()
+
+if __name__ == '__main__':
+    main()
